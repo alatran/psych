9552a3d82fd81184a29179c5f5082ed2d40de089
alatran <ala12tran21@gmail.com>
2025-06-10 08:35:58 -0400

    Update psych.py

--- psych.py
+++ psych.py
@@ -1,1 +1,10 @@
+import random
 
+def select(file):
+  with open(file, 'r') as f:
+    words = f.readlines()
+    words = [word.strip for word in words if word.strip()]
+  return random.choice(words) if words else None
+
+rand_word = select(exam2.txt)
+print(f"Word: {rand_word}")
